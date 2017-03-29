ba1f059353373e076d81335c45d4e70bf3c6fc1e
pigflymoon <pplflymoon2015@gmail.com>
2017-03-29 17:30:56 +1300

    #Add basename to the Route to solve url match issue:

--- server.babel.js
+++ server.babel.js
@@ -1,12 +1,20 @@
-import express from 'express';
+import express from 'express'
 import path from 'path';
 
-const app = express();
+const app = express()
 
-app.use('/', express.static('public'));
+app.use('/', express.static('public'))
 
+// 通常用于加载静态资源
+// app.use(express.static(__dirname + '/public'))
+
+// 在你应用 JavaScript 文件中包含了一个 script 标签
+// 的 index.html 中处理任何一个 route
 app.get('*', function (request, response){
     response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
+
 app.listen(process.env.PORT || 3000);
+
+
