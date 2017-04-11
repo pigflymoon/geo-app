6df5fdbc8988e39a5cc5e5935d9a4e4e47a7ccc9
pigflymoon <pplflymoon2015@gmail.com>
2017-04-11 14:58:50 +1200

    #Chat page  #server side and client side

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ import Quakes from './components/Pages/Quakes/Quakes'
 import Quake from './components/Pages/Quake/Quake'
 import Chat from './components/Pages/Chat/Chat'
 
+import io from 'socket.io-client'
+
+
+var socket = io('/chat');
+
+// Add a connect listener
+socket.on('connect', function (socket) {
+    console.log('Client side : Connected!');
+});
+
+
 render((
 
     <Router>
--- server.babel.js
+++ server.babel.js
@@ -1,20 +1,36 @@
 import express from 'express'
 import path from 'path';
+import http from 'http'
+import io from 'socket.io'
 
 const app = express()
+const server = http.createServer(app)
+const serverIo = io(server)
+const port = process.env.PORT || 3000
 
-app.use('/', express.static('public'))
+var chat = serverIo.of('/chat').on('connection',function (socket) {
+    console.log('server side: a user connected with id %s',socket.id);
+})
+
+
+server.listen(port, function () {
+    console.log('Server listening at port %d', port)
+
+});
+
+
+app.use('/', express.static('public'));
 
 // 通常用于加载静态资源
 // app.use(express.static(__dirname + '/public'))
 
 // 在你应用 JavaScript 文件中包含了一个 script 标签
 // 的 index.html 中处理任何一个 route
-app.get('*', function (request, response){
+app.get('*', function (request, response) {
     response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-})
+});
 
 
-app.listen(process.env.PORT || 3000);
+
 
 
