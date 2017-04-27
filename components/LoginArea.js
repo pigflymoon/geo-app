ebabdcf2842c6bb9ade6f930ec5e8bfa3c8270c6
pigflymoon <pplflymoon2015@gmail.com>
2017-04-27 14:36:16 +1200

    #login-area

--- components/LoginArea.js
+++ components/LoginArea.js
@@ -40,11 +40,12 @@ export default class LoginArea extends React.Component {
         return (
             <div className="login-area grid-row" onKeyDown={this.handleKeyDown}>
 
-                <div className="form grid grid-cell u-3of4">
+                <div className="form grid grid-cell u-475">
 
-                    <div className="unnamed"></div>
+                    <div className="grid-cell u-1of2 unnamed"><i className="fa fa-user-circle-o" aria-hidden="true"></i>
+                    </div>
                     <div className="grid-cell u-1of2">
-                        <h3 className="title">What's your nickname?</h3>
+                        <h4 className="title">What's your nickname?</h4>
                         <input onChange={this.handleChange} className="username" type="text" maxLength="14"
                                ref="username" placeholder="Your nick name"/>
                         <input onChange={this.handleEmail} className="email" type="text" placeholder="Your email address"/>
