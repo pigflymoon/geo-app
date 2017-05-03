bc0511be55906e0da09a231d922d5430b3b72072
pigflymoon <pplflymoon2015@gmail.com>
2017-05-03 15:33:20 +1200

    #delete public in server.babel.js

--- server.babel.js
+++ server.babel.js
@@ -43,7 +43,7 @@ server.listen(port, function () {
 });
 
 
-app.use('/', express.static('public'));
+app.use('/', express.static(__dirname));
 
 // 通常用于加载静态资源
 // app.use(express.static(__dirname + '/public'))
@@ -51,7 +51,7 @@ app.use('/', express.static('public'));
 // 在你应用 JavaScript 文件中包含了一个 script 标签
 // 的 index.html 中处理任何一个 route
 app.get('*', function (request, response) {
-    response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+    response.sendFile(path.resolve('index.html'))
 });
 
 
